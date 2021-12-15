32d9e896e190634aad117432400f451ef6e23a4b
Fariz Fahmi <farizfahmi674@gmail.com>
2021-12-15 19:27:20 +0700

    Refactor profileController

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -6,38 +6,36 @@ use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class profileController extends Controller
 {
-
-    // public function show($id)
-    // {
-    //     // $id = Auth::user()->id;
-
-    //     dd(Auth::user()->email);
-    //     $data = profile::find($id);
-    //     $eUser = User::where('profile_id', $id)->first();    
-    //     return view('admin/profile/index', compact('data', 'eUser'));
-    // }
-
     public function index(){
         $eUser = Auth::user();
         $data = Profile::find($eUser->profile_id);
         return view('admin/profile/index', compact('data', 'eUser'));
     }
 
-    public function update(Request $request, Profile $profile)
+    public function update(Request $request, Profile $profile, User $user)
     {
-        $attr = $request->all();
-        if($request->file('picture')) {
-            $photo = $request->file('picture');
-            $photoUrl = $photo->storeAs('foto', "{$request->name}.{$photo->extension()}");
+        if($request->has('picture')) {
+            $picture = $profile->picture;
+            if($picture != ''){File::delete("images/profile/" . $picture);}
+            
+            $image_name = time() . '.jpg';
+            $imageProfile = $request->picture;
+            $imageProfile->move(public_path() . '/images/profile/', $image_name);
+            $dataProfile = ['picture' => $image_name];
         } else {
-            $photoUrl = $profile->picture;
+            $dataProfile = [
+                'name' => $request->name,
+                'username' => $request->username, 
+                'bio' => $request->bio
+            ];
+            $dataUser = ['email' => $request->email];
+            $user->where('id', Auth::user()->id)->update($dataUser);
         }
-        $attr['picture']=$photoUrl;
-        // dd($attr);
-        $profile->update($attr);
-        return back()->withInput();
+        $profile->where('id' ,Auth::user()->profile_id)->update($dataProfile);
+        return back()->withInput()->with('success', 'Data berhasil diupdate ! !');
     }
 }
